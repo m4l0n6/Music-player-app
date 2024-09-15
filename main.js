090dd626d2e990390460997285dc60de0241f414
m4l0n6 <mahonglonghg123@gmail.com>
2024-09-15 14:53:20 +0700

    fix js

--- main.js
+++ main.js
@@ -1,19 +1,24 @@
 const $ = document.querySelector.bind(document);
 const $$ = document.querySelectorAll.bind(document);
 
-const heading = $('header h2')
-const cdImage = $('.cd-img')
+const heading = $('header h2');
+const cdImage = $('.cd-img');
 const cd = $('.cd');
-const audio = $('#audio')
-const playBtn = $('.toggle-play-btn')
-const player = $('.player')
-const progress = $('#progress')
-const prevButton = $('.prev-btn')
-const nextButton = $('.next-btn')
-const processMin = $('.process-min')
-const randomButton = $('.random-btn')
-const repeatButton = $('.reperat-btn')
-const playlist = $('.playlist')
+const audio = $('#audio');
+const playBtn = $('.toggle-play-btn');
+const player = $('.player');
+const progress = $('#progress');
+const prevButton = $('.prev-btn');
+const nextButton = $('.next-btn');
+const processMin = $('.process-min');
+const randomButton = $('.random-btn');
+const repeatButton = $('.reperat-btn');
+const playlist = $('.playlist');
+
+let audioContext;
+let source;
+let gainNode;
+let bassFilter;
 
 function formatTime(seconds) {
     const minutes = Math.floor(seconds / 60);  // Lấy số phút
@@ -29,7 +34,7 @@ const app = {
     songs: [
         {
             name: 'Everything Goes on',
-            author: 'Porter Robinson ',
+            author: 'Porter Robinson',
             img: './asset/img/everything-goes-on.jpg',
             path: './asset/mp3/y2mate.com - Everything Goes On  Porter Robinson Official Music Video  Star Guardian 2022.mp3'
         },
@@ -61,15 +66,14 @@ const app = {
             name: 'Goodbye To A World',
             author: 'Porter Robinson',
             img: './asset/img/goodbyetoaworld.jpg',
-            path: './asset/mp3/y2mate.com - Porter Robinson  Goodbye To A World Official Audio.mp3  '
+            path: './asset/mp3/y2mate.com - Porter Robinson  Goodbye To A World Official Audio.mp3'
         },
     ],
     
-    // Hàm in ra
-    render: function(){
+    render: function() {
         const htmls = this.songs.map((song, index) => {
             return `
-                <div class="song ${index === this.currentIndex ? 'active' : ''}" data-index=${index}>
+                <div class="song ${index === this.currentIndex ? 'active' : ''}" data-index="${index}">
                     <div class="song-img" style="background-image: url('${song.img}')"></div>
                     <div class="body">
                         <h3 class="title">${song.name}</h3>
@@ -79,24 +83,22 @@ const app = {
                         <i class="fa-solid fa-ellipsis"></i>
                     </div>
                 </div>
-            `
-        })
-        $('.playlist').innerHTML= htmls // in ra màn hình
+            `;
+        });
+        $('.playlist').innerHTML = htmls.join(''); // in ra màn hình
     },
 
-    // Hàm định nghĩa thuộc tính
-    definePropertires: function(){
+    definePropertires: function() {
         Object.defineProperty(this, 'currentSong', {
-            get: function(){
-                return this.songs[this.currentIndex]
+            get: function() {
+                return this.songs[this.currentIndex];
             }
-        })
+        });
     },
 
-    // Hàm sử lý sự kiện
-    handleEvents: function(){
-        const _this = this
-        const cdwidth = cd.offsetWidth; // độ dài của cd
+    handleEvents: function() {
+        const _this = this;
+        const cdWidth = cd.offsetWidth; // độ dài của cd
 
         const cdImgAnimate = cdImage.animate([
             { transform: 'rotate(0deg)' },    // Từ 0 độ
@@ -108,15 +110,18 @@ const app = {
         
         cdImgAnimate.pause();  // Tạm dừng ngay sau khi khởi tạo
         
-        // Xử lý phóng to thu/ nhỏ của đĩa nhạc
-        document.onscroll = function(){
+        // Xử lý phóng to thu/nhỏ của đĩa nhạc
+        document.onscroll = function() {
             const scrollTop = window.scrollY || document.documentElement.scrollTop; // lấy ra độ dài của trình duyệt khi cuộn trang
-            const newCdWidth = cdwidth -scrollTop; // Tính ra độ dài mới sau cuộn trang
+            const newCdWidth = cdWidth - scrollTop; // Tính ra độ dài mới sau cuộn trang
             cd.style.width = newCdWidth > 0 ? newCdWidth + 'px' : 0; // thay đổi độ dài của cd
-            cd.style.opacity = newCdWidth / cdwidth;
-        }
+            cd.style.opacity = newCdWidth / cdWidth;
+        };
 
         playBtn.onclick = function() {
+            initAudioContext();  // Khởi tạo AudioContext khi người dùng nhấp vào nút phát
+            setupAudioNodes();   // Thiết lập các node âm thanh
+            
             if (_this.isPlaying) {
                 audio.pause();  // Dừng nhạc
                 cdImgAnimate.pause();  // Dừng xoay đĩa
@@ -139,6 +144,7 @@ const app = {
             player.classList.remove('playing');  // Gỡ class khi dừng nhạc
             cdImgAnimate.pause();  // Dừng xoay đĩa khi nhạc tạm dừng
         };
+        
         // Khi tiến độ bài hát thay đổi
         audio.ontimeupdate = function() {
             if (audio.duration) {
@@ -150,153 +156,165 @@ const app = {
                 processMin.textContent = `${formatTime(audio.currentTime)}/${formatTime(audio.duration)}`;
             }
         };
-        
 
-        // Xử lý tua
-        progress.oninput = function(e){
-            const seektime  = audio.duration / 100 * e.target.value
-            audio.currentTime = seektime
-        }
-
-        // Chuyển bài hát
-        nextButton.onclick = function(){
-            _this.nextSong()
-            audio.play()
-            _this.render()
-            _this.scrollToActiveSong()
-        }
-
-        // Quay lại bài trước
-        prevButton.onclick = function(){
-            _this.prevSong()
-            audio.play()
-            _this.render()
-            _this.scrollToActiveSong()
-        }
-
-        // Sự kiện ngẫu nhiên bài hát
-        randomButton.onclick = function(){
-            _this.isRandom = !_this.isRandom
-            randomButton.classList.toggle('active', _this.randomButton)
-        }
-        // Xỷ lý phát lại lặp lại bài hát
-        repeatButton.onclick = function(){
-            _this.isRepeat = !_this.isRepeat
-            repeatButton.classList.toggle('active', _this.repeatButton)
-        }
-
-        // Sự kiện khi kết thúc bài hát
-        audio.onended = function(){
-            if(_this.isRepeat){
-                audio.play()
-            }else{
-                nextButton.click()
-            }
-        }
-
-        // Lắng nghe sự kiện khi nhấn vào danh sách
-        playlist.onclick = function(e) {
-            const songNode = e.target.closest('.song:not(.active)')
-
-            if(songNode || e.target.closest('.option')){
-                if(songNode){
-                    _this.currentIndex = Number(songNode.dataset.index)
-                    _this.loadCurrentSong()
-                    _this.render()
-                    audio.play()
-                }
-
-                if(e.target.closest('.option')){
-                    songNode.preventDefault()
-                }
-            }
-        };
-        
-    },
-
-    scrollToActiveSong: function(){
-        setTimeout(() => {
-            $('.song.active').scrollIntoView({
-                behavior: 'smooth',
-                block: 'nearest',
-            })
-        }, 300)
-    },
-
-    // Tải thông tin bài hát đầu tiên vào UI khi chạy ứng dụng
-    loadCurrentSong: function() {
-          // Kiểm tra xem currentSong có tồn tại không
-        heading.textContent = this.currentSong.name;
-        cdImage.style.backgroundImage = `url(${this.currentSong.img})`;
-        audio.src = this.currentSong.path;
-
-        // Đặt lại thời gian đã phát và thời gian tổng khi bài hát mới được tải
+        // Khi metadata của bài hát được tải
         audio.onloadedmetadata = function() {
             processMin.textContent = `0:00/${formatTime(audio.duration)}`;
         };
+
+        // Xử lý tua
+        progress.oninput = function(e) {
+            const seekTime = audio.duration / 100 * e.target.value;
+            audio.currentTime = seekTime;
+        };
+
+        // Chuyển bài hát
+        nextButton.onclick = function() {
+            if (_this.isRandom) {
+                _this.playRandomSong();
+            } else {
+                _this.nextSong();
+            }
+            audio.play();
+            _this.render();
+            _this.scrollToActiveSong();
+        };
+
+        // Quay lại bài trước
+        prevButton.onclick = function() {
+            if (_this.isRandom) {
+                _this.playRandomSong();
+            } else {
+                _this.prevSong();
+            }
+            audio.play();
+            _this.render();
+            _this.scrollToActiveSong();
+        };
+
+        // Sự kiện ngẫu nhiên bài hát
+        randomButton.onclick = function() {
+            _this.isRandom = !_this.isRandom;
+            randomButton.classList.toggle('active', _this.isRandom);
+        };
+
+        // Xử lý phát lại lặp lại bài hát
+        repeatButton.onclick = function() {
+            _this.isRepeat = !_this.isRepeat;
+            repeatButton.classList.toggle('active', _this.isRepeat);
+        };
+
+        // Sự kiện khi kết thúc bài hát
+        audio.onended = function() {
+            if (_this.isRepeat) {
+                audio.play();
+            } else {
+                nextButton.click();
+            }
+        };
+
+        // Xử lý nhấp vào danh sách bài hát
+        playlist.onclick = function(e) {
+            const songNode = e.target.closest('.song:not(.active)');
+
+            if (songNode || e.target.closest('.option')) {
+                // Hành động khi nhấn vào bài hát
+                if (songNode) {
+                    _this.currentIndex = parseInt(songNode.dataset.index, 10);
+                    _this.loadCurrentSong();
+                    audio.play();
+                }
+                // Hành động khi ấn vào tùy chọn của bài hát
+                if (e.target.closest('.option')) {
+                    // Xử lý tùy chọn bài hát nếu cần
+                }
+            }
+        };
     },
 
-    nextSong: function() {
-        if (this.isRandom) {
-            this.playRandomSong();
-        } else {
-            this.currentIndex++;
-            if (this.currentIndex >= this.songs.length) {
-                this.currentIndex = 0;  // Quay lại bài đầu
-            }
-            this.loadCurrentSong();
-            this.render();  // Cập nhật danh sách bài hát
-            this.scrollToActiveSong();  // Cuộn đến bài đang phát
-        }
+    scrollToActiveSong: function() {
+        setTimeout(() => {
+            $('.song.active').scrollIntoView({
+                behavior: 'smooth',
+                block: 'center'
+            });
+        }, 300);
     },
-    
-    prevSong: function() {
-        if (this.isRandom) {
-            this.playRandomSong();
-        } else {
-            this.currentIndex--;
-            if (this.currentIndex < 0) {
-                this.currentIndex = this.songs.length - 1;
-            }
-            this.loadCurrentSong();
-        }
+
+    // Tải thông tin bài hát hiện tại
+    loadCurrentSong: function() {
+        heading.textContent = this.currentSong.name;
+        cdImage.style.backgroundImage = `url('${this.currentSong.img}')`;
+        audio.src = this.currentSong.path;
     },
-    
-    playRandomSong: function(){
-        let newIndex
-        do{
-            newIndex = Math.floor(Math.random() * this.songs.length)
-        } while (newIndex === this.currentIndex)
+
+    // Đặt thời gian hiện tại của bài hát về 0
+    resetProgress: function() {
+        progress.value = 0;
+        processMin.textContent = `0:00/${formatTime(audio.duration)}`;
+    },
+
+    // Phát bài hát ngẫu nhiên
+    playRandomSong: function() {
+        let newIndex;
+        do {
+            newIndex = Math.floor(Math.random() * this.songs.length);
+        } while (newIndex === this.currentIndex);
         
-        this.currentIndex = newIndex
-        this.loadCurrentSong()
+        this.currentIndex = newIndex;
+        this.loadCurrentSong();
     },
 
-    // Hàm bắt đầu
+    // Chuyển đến bài tiếp theo
+    nextSong: function() {
+        this.currentIndex++;
+        if (this.currentIndex >= this.songs.length) {
+            this.currentIndex = 0;
+        }
+        this.loadCurrentSong();
+    },
+
+    // Quay lại bài trước
+    prevSong: function() {
+        this.currentIndex--;
+        if (this.currentIndex < 0) {
+            this.currentIndex = this.songs.length - 1;
+        }
+        this.loadCurrentSong();
+    },
+
+    // Khởi động ứng dụng
     start: function() {
-        // Định nghĩa các thuộc tính của object
         this.definePropertires();
-    
-        // Sử dụng Web Audio API để xử lý âm thanh
-        const audioContext = new (window.AudioContext)();
-        const source = audioContext.createMediaElementSource(audio);
-        const gainNode = audioContext.createGain();
-        const bassFilter = audioContext.createBiquadFilter();
-    
-        bassFilter.type = 'lowshelf';
-        bassFilter.frequency.value = 200; // Cắt bass tại 200 Hz
-        bassFilter.gain.value = -10; // Giảm 10dB tần số bass
-    
-        // Kết nối các node xử lý âm thanh
-        source.connect(bassFilter);
-        bassFilter.connect(gainNode);
-        gainNode.connect(audioContext.destination);
-    
-        // Xử lý sự kiện và render UI
         this.handleEvents();
         this.loadCurrentSong();
         this.render();
-    },
+    }
+};
+
+function initAudioContext() {
+    if (!audioContext) {
+        audioContext = new (window.AudioContext || window.webkitAudioContext)();
+    }
+}
+
+function setupAudioNodes() {
+    if (!audioContext) return;
+
+    // Tạo nguồn âm thanh và các node xử lý
+    source = audioContext.createMediaElementSource(audio);
+    gainNode = audioContext.createGain();
+    bassFilter = audioContext.createBiquadFilter();
+
+    // Cấu hình bộ lọc bass
+    bassFilter.type = 'lowshelf';
+    bassFilter.frequency.value = 200; // Tần số cắt bass
+    bassFilter.gain.value = -10; // Giảm cường độ bass
+
+    // Kết nối các node
+    source.connect(bassFilter);
+    bassFilter.connect(gainNode);
+    gainNode.connect(audioContext.destination);
 }
 
 app.start();
